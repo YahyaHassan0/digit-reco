f4a6b04b5b5a909cb23f7264af36920f49c31907
Yahya Hassan <yah.hassan3@gmail.com>
2024-10-14 01:45:11 +0300

    adding libraries

--- main.py
+++ main.py
@@ -3,24 +3,24 @@ import numpy as np
 import tensorflow as tf
 import matplotlib.pyplot as plt
 
-mnist = tf.keras.datasets.mnsit
+mnist = tf.keras.datasets.mnist
 (x_train, y_train),(x_test, y_test) = mnist.load_data()
 
 x_train = tf.keras.utils.normalize(x_train, axis=1)
-x_train = tf.keras.utils.normalize(x_test, axis=1)
+x_test = tf.keras.utils.normalize(x_test, axis=1)
 
-model = tf.keras.models.squential()
+model = tf.keras.models.Sequential()
 model.add(tf.keras.layers.Flatten(input_shape=(28,28)))
 model.add(tf.keras.layers.Dense(units=128, activation='relu'))
 model.add(tf.keras.layers.Dense(units=128, activation='relu'))
 model.add(tf.keras.layers.Dense(units=10, activation='softmax'))
 
-model.compile(optimizer='adam',loss='sparse_categorcial_crossentropy', metrics=['accuracy'])
+model.compile(optimizer='adam',loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
-model.fit(x_test,y_train,epochs=3)
+model.fit(x_train,y_train,epochs=3)
 
-accuracy,loss= model.evaluate(x_test,y_test)
-print(accuracy)
-print(loss)
+loss,accuracy= model.evaluate(x_test,y_test)
+print(f"accuracy is:{accuracy}")
+print(f"the loss is: {loss}")
 
 model.save('digits.model')
