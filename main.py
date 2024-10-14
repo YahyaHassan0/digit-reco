d185d0227d95588924ec8bf806d9720d1c755f17
Yahya Hassan <yah.hassan3@gmail.com>
2024-10-15 00:45:31 +0300

    changing the epochs to 10

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ model.add(tf.keras.layers.Dense(units=10, activation='softmax'))
 
 model.compile(optimizer='adam',loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
-model.fit(x_train,y_train,epochs=3)
+model.fit(x_train,y_train,epochs=10)
 
 loss,accuracy= model.evaluate(x_test,y_test)
 print(f"accuracy is:{accuracy}")
